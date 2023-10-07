7267f1746893540c1d2ca0f1e87871f6e2819591
Пащенко Владимир <vladhalavchenko@gmail.com>
2023-10-07 14:25:04 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long l;
+        boolean b;
+        float f;
+        short s = 1200;
+
+        f = s;
+        l = (long)(Math.random() * 200 - 100);
+        System.out.println("random: " + l);
+        s = (short)l;
+        l= (long)f;
+
+        System.out.println("f: " + f);
+        System.out.println("l: " + l);
+        System.out.println("s: " + s);
+
+        double y = Math.log(s) + Math.log(1) / Math.log(f) - Math.asin(l);
+        System.out.println("Значення y: " + y);
     }
 }
